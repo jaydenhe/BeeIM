073be89aae478e4b1c9882544f1225999f8229f3
Jayden He <jayden.he@qq.com>
2014-04-14 06:34:25 -0400

    add frame.go

---
+++ s2c/s2cserver_test.go
@@ -0,0 +1,46 @@
+package s2c
+
+import (
+	"testing"
+	"net"
+)
+
+func TestLogin(t *testing.T){
+	go Run()
+	
+	conn,err:= net.DialTCP("tcp4","127.0.0.1:1114")
+
+	if err != nil {
+		t.Failed("connect error")
+	}
+
+	frameW := new(Frame)
+	frameW.messageType = 25
+	frameW.protobufData = []byte{0x1,0x2,0x3,0x4}
+
+	frameR,err := ReadAFrame(conn)
+
+	if frameR.messageType != 25 {
+		t.Error("(25)messageType = ",frameR.messageType,"\nprotodata= ",frameR.protobufData)
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
